05b0aab49860daaa0f447b7743dc7df4cbd21077
DCAA\admin1014 <numan.gt@dcaa.gov.ae>
2023-08-28 11:17:03 +0400

    added label to the ngx-mat-input-tel

--- projects/ngx-mat-intl-tel-input/src/lib/ngx-mat-intl-tel-input.component.ts
+++ projects/ngx-mat-intl-tel-input/src/lib/ngx-mat-intl-tel-input.component.ts
@@ -123,6 +123,7 @@ export class NgxMatIntlTelInputComponent
   @Input() enableSearch = false;
   @Input() searchPlaceholder: string | undefined;
   @Input() describedBy = '';
+  @Input() label = '';
 
   @Input()
   get format(): PhoneNumberFormat {
